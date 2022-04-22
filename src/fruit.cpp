6de6db4a95adc37c07fdd37c1243d42ab1c58214
Ivan Fomin <fomin.id@phystech.edu>
2022-04-22 21:32:44 +0300

    fix

--- src/fruit.cpp
+++
@@ -1,8 +0,0 @@
-#include "../fruit.h"
-
-Fruit::Fruit() : x_(15), y_(15) { }
-
-void Fruit::DrawFruit(sf::Sprite& image_fruit, sf::RenderWindow& window, Field& field) {
-  image_fruit.setPosition(x_ * field.ts, y_ * field.ts);
-  window.draw(image_fruit);
-}
